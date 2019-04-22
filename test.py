0cc3f99f48bc0d27f44ab595a265c76be336ac5a
Anh Nguyen <anhnguyen@Anhs-MacBook-Pro.local>
2019-04-21 22:06:57 -0400

    add test param

--- test.py
+++ test.py
@@ -16,12 +16,12 @@ def create_service(PORT):
     sim = simulate.Simulator('131.96.155.202', 6789, PORT)
     return sim
 
-def start_service():
-    service = sim.run()
+def start_service(step=100):
+    service = sim.run(step=step)
 
 def release_service(sim):
     sim.release_service()
     
-sim = create_service(4567)
+sim = create_service(5555)
 start_service()
 release_service(sim)
